e79f9b7a554e6fbaea0c473d51221bb8daa0f5b4
Mohammed Al Hariri <mhdhariri80@gmail.com>
2021-05-05 14:06:14 +0300

    Initial commit

---
+++ app/src/main/java/com/example/hindcalc/HndMainActivity.kt
@@ -0,0 +1,98 @@
+package com.example.hindcalc
+
+import android.database.Cursor
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.provider.Contacts
+import android.util.Log
+import android.view.View
+import android.view.ViewGroup
+import android.widget.*
+import androidx.core.view.get
+import androidx.core.view.iterator
+import com.example.hindcalc.databinding.HndmainactivityBinding
+import java.lang.reflect.Array
+import java.util.ArrayList
+import kotlin.concurrent.thread
+import kotlin.math.E
+
+class HndMainActivity : AppCompatActivity() {
+private lateinit var binding:HndmainactivityBinding
+    private lateinit var sqlHelper:HndSqlHelper
+    var arrlist:ArrayList<String> = ArrayList()
+    var cursor:Cursor?=null
+    lateinit var ArrAdpt: CursorAdapter
+ val fromcursor: kotlin.Array<String> = arrayOf(HndSqlHelper.Player_Name,
+        HndSqlHelper.Player_Score,HndSqlHelper.Player_Final_Score)
+    val to: IntArray =
+            intArrayOf(R.id.pname, R.id.Sumtxt,R.id.FinalScore)
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = HndmainactivityBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        //ArrAdpt= ArrayAdapter(this, R.layout.playerinfo, R.id.pname, arrlist)
+        sqlHelper= HndSqlHelper(applicationContext)
+        cursor=sqlHelper.getPlayers()
+        ArrAdpt=SimpleCursorAdapter(this,R.layout.playerinfo,cursor,fromcursor,to,CursorAdapter.NO_SELECTION)
+        binding.Dynamic.adapter=ArrAdpt
+        ArrAdpt.notifyDataSetChanged()
+findViewById<Button>(R.id.Calc).setOnClickListener(){
+var mylist:ListView=ListView(applicationContext)
+    mylist=findViewById(R.id._dynamic) as ListView
+    //for (i in [0..mylist])
+
+    //Toast.makeText(applicationContext, mylist.get(0).id.toString(),Toast.LENGTH_LONG)
+
+    for(ET:View in mylist) {
+        //var ET:View=Etv //mylist.getChildAt(1)// as ViewGroup
+        var pdname: TextView = ET.findViewById<TextView>(R.id.pname)
+        var EDT: EditText = ET.findViewById<EditText>(R.id.PlayerScore)
+        var TxtV: TextView = ET.findViewById<TextView>(R.id.Sumtxt)
+        //Log.d("tag1","myString: ${EDT.text.toString()}")
+        //ET.findViewById<TextView>(R.id.FinalScore).setText(EDT.text.toString())
+
+        var nScore: Int
+        var fScor: Int
+if(EDT.text.isNotEmpty()) {
+    nScore = EDT.text.toString().toInt() + TxtV.text.toString().toInt()
+    ET.findViewById<TextView>(R.id.Sumtxt).setText(nScore.toString())
+    ET.findViewById<TextView>(R.id.FinalScore).setText(EDT.text.toString())
+
+    sqlHelper.UpdatePlayerInfo(pdname.text.toString(), EDT.text.toString().toInt(), nScore)
+    EDT.setText("")
+}
+    }
+}
+
+
+
+
+    }
+
+    fun AddPlayer(view: View) {
+        //SaveDataIntoMemeory()
+        //simpleAdapter = SimpleAdapter(this,datamap,R.layout.playerinfo,from,to)
+//binding.Dynamic.adapter=simpleAdapter
+//        simpleAdapter.notifyDataSetChanged()
+//        binding.PlyerName.setText("")
+
+        //arrlist.add(binding.PlyerName.text.toString())
+        //ArrAdpt = ArrayAdapter(this, R.layout.playerinfo, R.id.pname, arrlist)
+
+       // binding.Dynamic.adapter = ArrAdpt
+       // ArrAdpt.notifyDataSetChanged()
+
+        //----------------
+        sqlHelper= HndSqlHelper(applicationContext)
+        val res=sqlHelper.AddPlayerInfo(binding.PlyerName.text.toString(),0,0)
+        cursor=sqlHelper.getPlayers()
+        if(cursor==null){
+            Toast.makeText(this,"cursor null", Toast.LENGTH_LONG).show()
+        }else {
+            Toast.makeText(this, "cur not null", Toast.LENGTH_LONG).show()
+            ArrAdpt.changeCursor(cursor)
+            binding.Dynamic.adapter=ArrAdpt
+            ArrAdpt.notifyDataSetChanged()
+        }
+    }
+}
---
+++ app/src/main/java/com/example/hindcalc/HndSqlHelper.kt
@@ -0,0 +1,72 @@
+package com.example.hindcalc
+
+import android.content.ContentValues
+import android.content.Context
+import android.database.Cursor
+import android.database.sqlite.SQLiteDatabase
+import android.database.sqlite.SQLiteOpenHelper
+import android.util.Log
+import androidx.annotation.ContentView
+
+class HndSqlHelper(context: Context): SQLiteOpenHelper(context,"HndDataBase",null,1) {
+
+    companion object{
+        val Table_Name_HndInfo:String="HND_PLYER_INFO"
+        val Game_id:String="_id"
+        val Game_Turn:String="GAME_TURN"
+        //val Player_ID:String="_ID"
+        val Player_Name:String="PLAYER_NAME"
+        val Player_Score:String="PLAYER_SCORE"
+        val Player_Final_Score:String="PLAYER_RESULT"
+    }
+    val selctCols:kotlin.Array<String> = arrayOf(HndSqlHelper.Player_Name,
+            HndSqlHelper.Player_Score,HndSqlHelper.Player_Final_Score)
+
+    private  val SQL_CREATE_ENTRIES =
+        "CREATE TABLE ${Table_Name_HndInfo} ( ${Game_id} INTEGER PRIMARY KEY AUTOINCREMENT, ${Game_Turn} INTEGER NOT NULL," +
+                " ${Player_Name} TEXT NOT NULL, ${Player_Score} INTEGER,  ${Player_Final_Score} INTEGER)"
+
+    private val SQL_DELETE_ENTRIES = "DROP TABLE IF EXISTS ${Table_Name_HndInfo}"
+
+    override fun onCreate(db: SQLiteDatabase?) {
+        db?.execSQL(SQL_CREATE_ENTRIES)
+    }
+
+    override fun onUpgrade(db: SQLiteDatabase?, oldVersion: Int, newVersion: Int) {
+        db?.execSQL(SQL_DELETE_ENTRIES)
+        onCreate(db)
+    }
+
+    override fun onDowngrade(db: SQLiteDatabase?, oldVersion: Int, newVersion: Int) {
+        onUpgrade(db,oldVersion,newVersion)
+    }
+    fun AddPlayerInfo(name:String,GScore:Int,SumScore:Int):Long{
+    val db=this.writableDatabase
+    val v=ContentValues().apply {
+     put(Player_Name,name)
+     put(Player_Score,SumScore)
+     put(Player_Final_Score,GScore)
+     put(Game_Turn,1)
+    }
+
+        var a =db.insert(Table_Name_HndInfo,null,v)
+        Log.d("tag1","Insert:"+a.toString())
+return a
+    }
+
+    fun getPlayers(): Cursor? {
+        val db = this.readableDatabase
+        return  db.query(Table_Name_HndInfo,null,null,null,null,null,null)
+    }
+    fun UpdatePlayerInfo(name:String,GScore:Int,SumScore:Int)
+    {
+        val db=this.writableDatabase
+        val v=ContentValues().apply {
+            put(Player_Name,name)
+            put(Player_Score,GScore)
+            put(Player_Final_Score,SumScore)
+            put(Game_Turn,1)
+        }
+        val a=db.update(Table_Name_HndInfo,v, "${Player_Name} = '${name}'",null)
+    }
+}
