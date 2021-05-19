aaee83f048274df1a990e5ee8dd853fdfcf31e21
Mohammed Al Hariri <mhdhariri80@gmail.com>
2021-05-19 17:38:43 +0300

    Try to color the higher and the lower score

--- app/src/main/java/com/example/hindcalc/HndMainActivity.kt
+++ app/src/main/java/com/example/hindcalc/HndMainActivity.kt
@@ -1,6 +1,8 @@
 package com.example.hindcalc
 
+import android.content.res.Configuration
 import android.database.Cursor
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.provider.Contacts
@@ -26,13 +28,18 @@ private lateinit var binding:HndmainactivityBinding
         HndSqlHelper.Player_Score,HndSqlHelper.Player_Final_Score)
     val to: IntArray =
             intArrayOf(R.id.pname, R.id.FinalScore, R.id.Sumtxt)
+    lateinit var mylist:ListView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = HndmainactivityBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        mylist=ListView(applicationContext)
+        mylist=findViewById(R.id._dynamic) as ListView
         //ArrAdpt= ArrayAdapter(this, R.layout.playerinfo, R.id.pname, arrlist)
         sqlHelper= HndSqlHelper(applicationContext)
         cursor=sqlHelper.getPlayers()
+        mylist.setBackgroundColor(Color.WHITE)
 //            .readableDatabase.query(
 //            HndSqlHelper.Table_Name_HndInfo,null,null,null,null,null,
 //            HndSqlHelper.Player_Final_Score +" ASC")
@@ -40,14 +47,12 @@ private lateinit var binding:HndmainactivityBinding
         ArrAdpt=SimpleCursorAdapter(this,R.layout.playerinfo,cursor,fromcursor,to,CursorAdapter.NO_SELECTION)
         binding.Dynamic.adapter=ArrAdpt
         ArrAdpt.notifyDataSetChanged()
-        var mylist:ListView=ListView(applicationContext)
-        mylist=findViewById(R.id._dynamic) as ListView
+
 
         findViewById<Button>(R.id.Calc).setOnClickListener(){
     //for (i in [0..mylist])
 
     //Toast.makeText(applicationContext, mylist.get(0).id.toString(),Toast.LENGTH_LONG)
-
     for(ET:View in mylist) {
         //var ET:View=Etv //mylist.getChildAt(1)// as ViewGroup
         var pdname: TextView = ET.findViewById<TextView>(R.id.pname)
@@ -58,6 +63,7 @@ private lateinit var binding:HndmainactivityBinding
 
         var nScore: Int
         var fScor: Int
+
         try {
             if (EDT.text.isNotEmpty()) {
                 nScore = EDT.text.toString().toInt() + TxtV.text.toString().toInt()
@@ -73,17 +79,36 @@ private lateinit var binding:HndmainactivityBinding
                 //binding.Dynamic.adapter=ArrAdpt
                 ArrAdpt.notifyDataSetChanged()
                 EDT.setText("")
+                //ET.setBackgroundColor(Color.WHITE)
             }
         }catch(e:Exception){
             Toast.makeText(this, "Error: ${e.toString()}", Toast.LENGTH_LONG).show()
         }
 
     }
+            var ln0 =mylist.getItemIdAtPosition(0).toInt()
+            //var ln0=mylist.adapter.getItemId(0).toInt()
+            mylist.setBackgroundColor(Color.WHITE)
+            //Log.d("tag2","First: ${mylist.get(0).findViewById<TextView>(R.id.pname).text.toString()} Last:${mylist.get(mylist.count-1).findViewById<TextView>(R.id.pname).text.toString()} ")
+            Log.d("tag2","First: ${ArrAdpt.getView(0,null, mylist).findViewById<TextView>(R.id.pname).text.toString()} ")
+
+            //mylist.get(0).setBackgroundColor(Color.GREEN)
+
+            val cur:Cursor= mylist.getItemAtPosition(0) as Cursor
+            val i= cursor?.position ?: Int
+            //val ii=i as Int
+            val v:View= ArrAdpt.getView(0,mylist.get(mylist[0].top) as View, mylist)
+                    //mylist.get(mylist[0].top) as View
+            v.setBackgroundColor(Color.GREEN)
+            //Toast.makeText(applicationContext,"${mylist.get(0).findViewById<TextView>(R.id.pname).text.toString()}")
+            mylist.get(mylist.count-1).setBackgroundColor(Color.RED)
 
 }
 
         findViewById<Button>(R.id.newGame).setOnClickListener{
             //Toast.makeText(this, "new game", Toast.LENGTH_LONG).show()
+            mylist.setBackgroundColor(Color.WHITE)
+            //mylist.refreshDrawableState()
             for(ET:View in mylist) {
                 //var ET:View=Etv //mylist.getChildAt(1)// as ViewGroup
                 var pdname: TextView = ET.findViewById<TextView>(R.id.pname)
@@ -145,4 +170,5 @@ private lateinit var binding:HndmainactivityBinding
     fun newGame(){
 
     }
-}
+
+    }
